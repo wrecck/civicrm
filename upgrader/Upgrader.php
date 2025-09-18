a7ac288a503049f9cb01454ae699a5e1663ccc3e
wrecck <wrecck@gmail.com>
2025-09-19 01:48:53 +0800

    extension update #6

--- upgrader/Upgrader.php
+++ upgrader/Upgrader.php
@@ -7,37 +7,22 @@ use CRM_UnitLedger_ExtensionUtil as E;
  */
 class CRM_UnitLedger_Upgrader extends CRM_Extension_Upgrader_Base {
 
-  /**
-   * Perform actions after install
-   */
   public function postInstall() {
     $this->registerCiviRulesActions();
   }
 
-  /**
-   * Perform actions after enable
-   */
   public function postEnable() {
     $this->registerCiviRulesActions();
   }
 
-  /**
-   * Example of a versioned upgrade step (optional)
-   * Called when upgrading from version 1.0.0 to 1.1.0
-   */
-  public function upgrade_1100() {
+  public function upgrade_1001() {
     $this->registerCiviRulesActions();
     return TRUE;
   }
 
-  /**
-   * Register CiviRules actions from JSON file
-   */
   private function registerCiviRulesActions() {
     if (!method_exists('CRM_Civirules_Utils_Upgrader', 'insertActionsFromJson')) {
-      CRM_Core_Error::debug_log_message(
-        'CiviRules not enabled; skipping Unit Ledger action registration.'
-      );
+      CRM_Core_Error::debug_log_message('CiviRules not enabled (UnitLedger): skipping action registration');
       return;
     }
 
@@ -45,9 +30,7 @@ class CRM_UnitLedger_Upgrader extends CRM_Extension_Upgrader_Base {
     if (file_exists($jsonFile)) {
       CRM_Civirules_Utils_Upgrader::insertActionsFromJson($jsonFile);
     } else {
-      CRM_Core_Error::debug_log_message(
-        "Unit Ledger Upgrader could not find civirules_actions.json at {$jsonFile}"
-      );
+      CRM_Core_Error::debug_log_message("UnitLedger: civirules_actions.json not found at {$jsonFile}");
     }
   }
 }
